c4213a75cfb5d5cf408703631940ed833e89abc7
Mattia Failla <mattiafailla@hotmail.it>
2021-10-11 20:43:48 +0200

    Self install script

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,8 @@ COPY . .
 RUN npm install
 RUN npm install -g pm2
 
+RUN chmod +x ./perform_app_init.sh 
+RUN ./perform_app_init.sh
+
 ENV WS_SECRET="metermonitorsecret"
 CMD ["pm2-runtime", "start", "app.json"]
---
+++ perform_app_init.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Perorm app init, set ip address during build time
+
+PUBLIC_IP_ADDR=$(curl ipinfo.io/ip)
+
+echo """
+[
+  {
+    \"name\": \"node-app\",
+    \"script\": \"app.js\",
+    \"log_date_format\": \"YYYY-MM-DD HH:mm Z\",
+    \"merge_logs\": false,
+    \"watch\": false,
+    \"max_restarts\": 10,
+    \"exec_interpreter\": \"node\",
+    \"exec_mode\": \"fork_mode\",
+    \"env\": {
+      \"NODE_ENV\": \"production\",
+      \"RPC_HOST\": \"$PUBLIC_IP_ADDR\",
+      \"RPC_PORT\": \"8545\",
+      \"LISTENING_PORT\": \"30303\",
+      \"INSTANCE_NAME\": \"Jelly\",
+      \"CONTACT_DETAILS\": \"https://www.jellypool.xyz\",
+      \"WS_SERVER\": \"ws://meter-stats-server.nextblu.com:3030\",
+      \"WS_SECRET\": \"metermonitorsecret\",
+      \"VERBOSITY\": 2
+    }
+  }
+]
+""" >| app.json
