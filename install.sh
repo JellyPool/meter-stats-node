b7cabdd11b8ebf90944a4273fff4f0f0c96a0b61
itsMett <mattiafailla@hotmail.it>
2022-04-02 04:13:26 +0200

    Implement base install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+# Delete the meter-stats repo folder if exists
+echo "Deleting repository folder..."
+rm -rf ./meter-stats-node/
+
+# Pull the git repo
+echo "Cloning new repository version.."
+git clone https://github.com/nextblu/meter-stats-node.git
+
+# Terminate the node_monitor container
+echo "Terminating node_monitor container.."
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=node_monitor --format="{{.ID}}"))
+
+# Initialize the new container installation
+echo "Building new node_monitor version.."
+docker build meter-stats-node/Dockerfile -t node_monitor
+docker run -d -t node_monitor
+
+# Done!
+echo "Done! Thanks for your contribution! \nYou can find your node here: https://meterstats.jellypool.xyz/"
